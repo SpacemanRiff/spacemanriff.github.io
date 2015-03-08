d191e007950145a69389d948d17674ef5dbaec65
Scott Troutman <troutmansm@gmail.com>
2015-03-08 15:35:42 -0500

    holding shit makes background go faster

--- backgroundmove.js
+++ backgroundmove.js
@@ -1,15 +1,19 @@
-const xscale = 0.0125;
-const yscale = 0.0125;
+const xdefscale = 0.0125;
+const ydefscale = 0.0125;
 const xdecscalefast = xscale / 4;
 const ydecscalefast = yscale / 4;
 const xmax = 10; const ymax = 10;
-    
+
+var xscale = xdefscale;
+var yscale = xdefscale;    
 var xpos = 0; var ypos = 0;
 var xold = 0; var yold = 0; 
 var xmove = 0; var ymove = 0;        
 var xdec = 0; var ydec = 0;        
 var xdecscale = xscale / 16;
 var ydecscale = yscale / 16;
+var isclicked = false;
+var isshifted = false;
 
 document.addEventListener('mousemove', function (event){
     if (window.event) event = window.event;  
@@ -48,18 +52,50 @@ document.addEventListener('mousemove', function (event){
     var header = document.getElementById('header');
 }, false);
 
-document.addEventListener('mousedown', function (event) {  
-    xdecscale = xscale / 4;
-    ydecscale = yscale / 4;
-    
-    calcDec();
+document.addEventListener('mousedown', function (event) {
+    if(!isclicked){
+        xdecscale = xscale / 2;
+        ydecscale = yscale / 2; 
+        isclicked = true;    
+        calcDec();
+        console.log("down"); 
+    }
 }, false);
 
 document.addEventListener('mouseup', function (event) {  
-    xdecscale = xscale / 16;
-    ydecscale = yscale / 16;
-    
-    calcDec();
+    if(isclicked){
+        xdecscale = xscale / 16;
+        ydecscale = yscale / 16;    
+        isclicked = false;        
+        calcDec();    
+        console.log("up");
+    }
+}, false);
+
+document.addEventListener('keydown', function (event) {  
+    var key = event.keyCode ? event.keyCode : event.which;
+    if(key == 16){
+        if(!isshifted){
+            xscale = xdefscale * 8;
+            yscale = ydefscale * 8;    
+            isshifted = true; 
+            calcDec();
+            console.log("down");   
+        }
+    }
+}, false);
+
+document.addEventListener('keyup', function (event) {  
+    var key = event.keyCode ? event.keyCode : event.which;
+    if(key == 16){
+        if(isshifted){
+            xscale = xdefscale;
+            yscale = ydefscale;    
+            isshifted = false;;   
+            calcDec(); 
+            console.log("up");  
+        }
+    }
 }, false);
 
 setInterval(function() {moveSpace()}, 10);
